3dccb5eeda24d1f00deab86dff40ca944d376511
WolfStorm <longredtie@yandex.ru>
2020-09-12 16:03:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP_Lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            bool bl = true;
+            Console.WriteLine($"Bool:\t\t{bl}");
+            byte bt = 12;
+            Console.WriteLine($"Byte:\t\t{bt}");
+            sbyte sbt = -10;
+            Console.WriteLine($"sByte:\t\t{sbt}");
+            char ch = 'V';
+            Console.WriteLine($"Char:\t\t{ch}");
+            decimal dcml = 7.921M;
+            Console.WriteLine($"Decimal:\t{dcml}");
+            double dbl = 5.33;
+            Console.WriteLine($"Double:\t\t{dbl}");
+            float flt = 10.22F;
+            Console.WriteLine($"Float:\t\t{flt}");
+            int nt = 5;
+            Console.WriteLine($"Int:\t\t{nt}");
+            uint unt = 10;
+            Console.WriteLine($"uInt:\t\t{unt}");
+            long lng = -101010;
+            Console.WriteLine($"Long:\t\t{lng}");
+            ulong ulng = 132013120;
+            Console.WriteLine($"uLong:\t\t{ulng}");
+            short shrt = -1;
+            Console.WriteLine($"Short:\t\t{shrt}");
+            ushort ushrt = 100;
+            Console.WriteLine($"uShort:\t\t{ushrt}");
+
+        }
+    }
+}
